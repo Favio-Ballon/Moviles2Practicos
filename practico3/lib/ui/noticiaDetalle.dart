8727483a14d2141c5bf47984fa334daa1afccb84
favio-ballon <favioballon@gmail.com>
2024-10-05 07:48:47 -0400

    .

--- practico3/lib/ui/noticiaDetalle.dart
+++ practico3/lib/ui/noticiaDetalle.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:practico3/models/article.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NoticiaDetalle extends StatelessWidget {
   final Article article;
 
   NoticiaDetalle({required this.article});
+
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,16 +25,31 @@ class NoticiaDetalle extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Text(article.title,
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
             Image.network(article.urlToImage),
             SizedBox(height: 16.0),
             SizedBox(height: 8.0),
             Text(article.description),
             SizedBox(height: 8.0),
-            Text('Published at: ${article.publishedAt}'),
+            Text('Publicado el: ${article.publishedAt}'),
             SizedBox(height: 8.0),
-            Text(article.content),
-            //TODO
-            Text(article.url)
+            Text(article.content,
+                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400)),
+            //Colorea el link de azul y subrayado
+            GestureDetector(
+              onTap: () {
+                _launchURL(article.url); // Abre el enlace en el navegador
+              },
+              child: const Text(
+                'Link de la noticia',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
           ],
         ),
       ),
